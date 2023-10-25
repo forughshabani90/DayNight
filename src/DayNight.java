98a5177253762efa3e97c105e756eff5202d15f6
forughshabani90 <forugh.shabani.90@gmail.com>
2023-10-25 04:49:57 -0700

    fc

---
+++ src/DayNight.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class DayNight {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        if (scanner > 7 & scanner < 19) {
+            System.out.println("day");
+        else if (scanner > 19 & scanner =<6){
+                System.out.println("night");
+            }
+
+        }
+    }
